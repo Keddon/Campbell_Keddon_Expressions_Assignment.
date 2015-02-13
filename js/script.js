b2aee1f94db21ed9540612239726dc36d6a841d3
Keddon Campbell <mrkfed101@gmail.com>
2015-02-12 21:29:10 -0500

    Assignment
    Initial Sync

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/**
+ * Created by keddon campbell on 2/12/15 Expression Assignment.
+ */
+
+
+alert("Welcome to the very first calculator created by Keddon");
+
+
+var firstNumber = prompt("What is your first number?");
+var secondNumber = prompt("what is your second number?");
+var operator = prompt("Please select an operation. Your options are: 0+ 1* 2- 3/ ");
+var calculation;
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*if (operator = "/" ) {
+    calculation = Number(firstNumber) / Number(secondNumber);}
+    else if (operator = "*") {
+        calculation = Number(firstNumberv) * Number(secondNumber);}
+        else if (operator = "-") {
+            calculation = Number(firstNumber) - Number(secondNumber);}
+            else if (operator = "+") {
+                calculation = Number(firstNumber) + Number(secondNumber);
+}
+
+var text =
+*/
+console.log(calculation);
+alert("The answer of " +Number(firstNumber) + operator + Number(secondNumber)+ " is " +calculation);
