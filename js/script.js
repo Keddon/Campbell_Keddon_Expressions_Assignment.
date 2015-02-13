a79d41ee26890fcfec6e76e055fb68737e0257f7
Keddon Campbell <mrkfed101@gmail.com>
2015-02-12 21:40:08 -0500

    Update
    I am using switch/cases to find what operation the user wants to take.

--- js/script.js
+++ js/script.js
@@ -12,10 +12,22 @@ var operator = prompt("Please select an operation. Your options are: 0+ 1* 2- 3/
 var calculation;
 
 
-
-
-
-
+switch (operator) {  //added a set of case statement to see if they make things simple
+    case 1 : "/"
+        calculation = firstNumber / secondNumber;
+        break;
+    case 2 : "*"
+        calculation = firstNumber * secondNumber;
+        break;
+    case 3 : "-"
+        calculation = firstNumber - secondNumber;
+        break;
+    case 4 : "+"
+        calculation = firstNumber + secondNumber;
+        break;
+    default :
+        "something may have went wrong, lets try again"
+}
 
 
 
